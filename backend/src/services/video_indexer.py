5945a3592ae55678c5e559db150d9fa7e527913a
Shubham-rawat0 <rawat07shubham@gmail.com>
2026-04-17 09:48:04 +0530

    video indexeing

--- backend/src/services/video_indexer.py
+++ backend/src/services/video_indexer.py
@@ -1,11 +1,148 @@
-import json
 import os
 import logging
-import re
-from typing import Dict ,Any ,List
-from langchain_openai import AzureChatOpenAI , AzureOpenAIEmbeddings
-from langchain_community.vectorstores import AzureSearch
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import  SystemMessage, HumanMessage
-from backend.src.graph.state import VideoAuditState , ComplianceIssue
-from backend.src.services.video_indexer import VideoIndexerService
+import requests
+import yt_dlp
+import time
+from azure.identity import DefaultAzureCredential
+
+logger = logging.getLogger("video-indexer")
+
+class VideoIndexerService:
+
+    def __init__(self):
+
+        self.account_id = os.getenv("AZURE_VI_ACCOUNT_ID")
+        self.location = os.getenv("AZURE_VI_LOCATION")
+        self.subscription_id = os.getenv("AZURE_SUBSCRIPTION_ID")
+        self.resource_group=os.getenv("AZURE_RESOURCE_GROUP")
+        self.vi_name= os.getenv("AZURE_VI_NAME")
+        self.credential = DefaultAzureCredential()
+    
+    def get_access_token(self):
+
+        try:
+            token_object = self.credential.get_token("https://management.azure.com/.default")
+
+            return token_object.token
+        
+        except Exception as e:
+            logger.error(f"failed to get axure token, {e}")
+            raise
+
+    def get_account_token(self,arm_access_token):
+
+        url = (
+            f"https://management.azure.com/subscriptions/{self.subscription_id}"
+            f"/resourceGroups/{self.resource_group}"
+            f"/providers/Microsoft.VideoIndexer/accounts/{self.vi_name}"
+            f"/generateAccessToken?api-version=2024-01-01"
+        )
+
+        headers = {"Authorization": f"Bearer {arm_access_token}"}
+        payload = {"permissionType": "Contributor", "scope": "Account"}
+        response = requests.post(url, headers=headers, json=payload)
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to get VI Account Token: {response.text}")
+        
+        return response.json().get("accessToken")
+    
+    def download_youtube_video(self,url , output_path):
+
+        logger.info("downloading yt video",url)
+
+        ydl_opts = {
+            'format': 'best',
+         'outtmpl': output_path, 
+         'quiet': False,
+         'no_warnings': False,
+         'extractor_args': {'youtube': {'player_client': ['android', 'web']}},
+         'http_headers': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+            }
+        }
+
+        try : 
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+
+            logger.info("download complete")
+
+        except Exception as e:
+            raise Exception(f"Youtube video download failed")
+        
+    def upload_video(self, video_path , video_name):
+
+        arm_token = self.get_access_token()
+        vi_token = self.get_access_token(arm_token)
+
+        api_url = f"https://api.videoindexer.ai/{self.location}/Accounts/{self.account_id}/Videos"
+
+        parms = {
+            "accessToken": vi_token,
+            "name": video_name,
+            "privacy": "Private",
+            "indexingPreset": "Default",
+        }
+
+        logger.info(f"Uploading file {video_path} to Azure...")
+
+        with open(video_path,"rb") as video_file:
+            files= {"file":video_file}
+            response = requests.post(api_url , params=parms , files= files)
+        
+        if response.status_code != 200:
+            raise Exception(f"Azure Upload Failed: {response.text}")
+            
+        return response.json().get("id")
+    
+    def wait_for_processing(self , video_id):
+        logger.info(f"waiting for the video {video_id}")
+
+        while True:
+            arm_token = self.get_access_token()
+            vi_token = self.get_account_token(arm_token)
+            
+            url = f"https://api.videoindexer.ai/{self.location}/Accounts/{self.account_id}/Videos/{video_id}/Index"
+            params = {"accessToken": vi_token}
+
+            response = requests.get(url, params=params)
+            data = response.json()
+
+            state= data.get("state")
+
+            if state == "Processed":
+                return data
+            
+            elif state == "Failed":
+                raise Exception("Video indexing failed")
+            
+            elif state == "Quarantined":
+                raise Exception("Copyright content voilation")
+
+            logger.info(f"Status: {state}... waiting 30s")
+            time.sleep(30)
+    
+    def extract_data(self , vi_json):
+
+        transcript_lines = []
+
+        for v in vi_json.get("videos",[]):
+            for insight in v.get("insights",{}).get("transcript",[]):
+                transcript_lines.append(insight.get("text"))
+        
+        ocr_lines = []
+
+        for v in vi_json.get("videos",[]):
+            for insight in v.get("insights",{}).get("ocr",[]):
+                ocr_lines.append(insight.get("text"))
+            
+        return{
+            "transcript": " ".join(transcript_lines),
+            "ocr_text": ocr_lines,
+            "video_metadata": {
+                "duration": vi_json.get("summarizedInsights", {}).get("duration", {}).get("seconds"),
+                "platform": "youtube"
+            }
+        }
+
